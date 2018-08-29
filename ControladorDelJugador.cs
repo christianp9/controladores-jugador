82bc9bf90db6b68a66eed69727e6cea41bc4a739
christianp9 <pearson3135@gmail.com>
2018-08-29 12:49:31 -0500

    controladores

---
+++ ControladorDelJugador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladordeljugador : MonoBehaviour
+{
+    Rigidbody RB;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float movimientoH = Input.GetAxis("Horizontal");
+        float movimientoV = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoH,0,movimientoV);
+        RB.AddForce(movimiento);
+    }
+
+}
